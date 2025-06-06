b638abef92c09aa1086e470a21d46d52529b29f3
charl-baum <charlottebaumgart61@gmail.com>
2025-06-06 23:27:34 +0200

    Tentatives pt arrivée (faux)

--- minijam186/scripts/player.gd
+++ minijam186/scripts/player.gd
@@ -5,6 +5,10 @@ signal throw_ball
 
 var angle_move_speed = 2
 
+var gravity : float = ProjectSettings.get_setting("physics/2d/default_gravity")
+var force = 500
+@onready var impact_point: Sprite2D = $Impact_point
+
 func _process(delta: float) -> void:
 	if Input.is_action_pressed("left"):
 		if arrow.rotation_degrees > -90 :
@@ -14,3 +18,20 @@ func _process(delta: float) -> void:
 			arrow.rotation_degrees += angle_move_speed
 	if Input.is_action_just_pressed("throw"):
 		throw_ball.emit(arrow.rotation_degrees)
+		
+		
+	#En attente de la correction de la func calculate impact position
+	var impact = calculate_impact_position(self.position)
+	impact_point.position.x = impact[0]
+	impact_point.position.y = impact[1]
+
+
+
+	
+#FAUX FAUX FAUX à recalculer
+func calculate_impact_position(position_juggler):
+	var impact_x = 1/gravity*(-force+gravity*self.position[0]+sqrt(force*force-2*gravity*self.position[1]))
+	print(-force+gravity*self.position[0])
+	var impact_y = position_juggler[1]
+	var impact = [impact_x,impact_y]
+	return impact
