af2e574f053a871a053cf51f9e124a40666bb6c9
SolennEstier <solenn.estier@gmail.com>
2025-06-06 21:16:24 +0200

    added detection shape for juggler

--- minijam186/scripts/juggler.gd
+++ minijam186/scripts/juggler.gd
@@ -1,1 +1,12 @@
 extends Node2D
+
+signal ball_caught
+
+func _process(delta: float) -> void:
+	pass
+	
+	
+
+
+func _on_juggler_detection_area_body_entered(body: Node2D) -> void:
+	ball_caught.emit(body)
--- minijam186/scripts/main.gd
+++ minijam186/scripts/main.gd
@@ -1,1 +1,10 @@
 extends Node2D
+
+
+func _on_juggler_body_ball_caught(body) -> void:
+	# faire disparaitre la balle concernée, genre body.queue_free()
+	# Lancer animation de bravo + son de bravo
+	pass # Replace with function body.
+
+func ball_missed():
+	pass
