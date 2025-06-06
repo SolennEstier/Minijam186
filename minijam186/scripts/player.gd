361fc0d17c6abeea9a6f50235f54761d83221741
SolennEstier <solenn.estier@gmail.com>
2025-06-06 21:07:31 +0200

    added arrow moving for player

--- minijam186/scripts/player.gd
+++ minijam186/scripts/player.gd
@@ -1,1 +1,16 @@
 extends CharacterBody2D
+@onready var arrow: Node2D = $Arrow
+
+signal throw_ball
+
+var angle_move_speed = 2
+
+func _process(delta: float) -> void:
+	if Input.is_action_pressed("left"):
+		if arrow.rotation_degrees > -90 :
+			arrow.rotation_degrees -= angle_move_speed
+	if Input.is_action_pressed("right"):
+		if arrow.rotation_degrees < 0 :
+			arrow.rotation_degrees += angle_move_speed
+	if Input.is_action_just_pressed("throw"):
+		throw_ball.emit()
