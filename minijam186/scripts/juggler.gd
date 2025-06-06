8d439f3a54012c618150d5c5ade6bc14ab0e7e1d
SolennEstier <solenn.estier@gmail.com>
2025-06-06 22:35:08 +0200

    added ball resetting when caught

--- minijam186/scripts/juggler.gd
+++ minijam186/scripts/juggler.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 signal ball_caught
 
+var move_speed = 1
+
 func _process(delta: float) -> void:
 	pass
 	
@@ -14,5 +16,5 @@ func _on_juggler_detection_area_body_entered(body: Node2D) -> void:
 
 func move_towards_ball(ball_final_position):
 	var x_speed = ball_final_position - self.position.x
-	self.velocity = Vector2(x_speed, 0)
+	self.velocity = Vector2(x_speed, 0).normalized()*move_speed
 	pass
--- minijam186/scripts/main.gd
+++ minijam186/scripts/main.gd
@@ -1,21 +1,38 @@
 extends Node2D
 @onready var player_body: CharacterBody2D = $player_body
 @onready var ball: RigidBody2D = $Ball
+var ball_scene = preload("res://scenes/ball.tscn")
+var start_ball_position: Vector2
+@onready var active_ball = ball
 
+
+func _ready():
+	start_ball_position = ball.position
+	pass
 
 func _on_juggler_body_ball_caught(body) -> void:
 	print('attrapé !')
 	body.queue_free()
 	# Lancer animation de bravo + son de bravo
+	# Spawn a new ball
+	var new_ball = ball_scene.instantiate()
+	add_child(new_ball)
+	new_ball.position = start_ball_position
+	active_ball = new_ball
 	pass # Replace with function body.
 
 func ball_missed(body):
 	body.queue_free()
 	print("Raté !")
+	## Remplacer ce qui suit par ce qui arrive quand le jongleur rate
+	var new_ball = ball_scene.instantiate()
+	add_child(new_ball)
+	new_ball.position = start_ball_position
+	active_ball = new_ball
 
 
 func _on_player_body_throw_ball(angle) -> void:
-	ball.throw(angle)
+	active_ball.throw(angle)
 
 
 func _on_down_boundary_body_entered(body: Node2D) -> void:
