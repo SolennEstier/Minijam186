dd0a2e658bd62c8d03f3aa52473a63bab27f9ffc
SolennEstier <solenn.estier@gmail.com>
2025-06-06 21:43:49 +0200

    Update juggler.gd

--- minijam186/scripts/juggler.gd
+++ minijam186/scripts/juggler.gd
@@ -10,3 +10,9 @@ func _process(delta: float) -> void:
 
 func _on_juggler_detection_area_body_entered(body: Node2D) -> void:
 	ball_caught.emit(body)
+	self.velocity = Vector2(0,0)
+
+func move_towards_ball(ball_final_position):
+	var y_speed = ball_final_position - self.position.x
+	self.velocity = Vector2()
+	pass
