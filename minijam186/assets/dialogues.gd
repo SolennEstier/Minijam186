35fe6d4c1cce20700d51d70c0142efd49b69aba1
SolennEstier <solenn.estier@gmail.com>
2025-06-07 22:32:13 +0200

    changed text

--- minijam186/assets/dialogues.gd
+++ minijam186/assets/dialogues.gd
@@ -6,7 +6,7 @@ class_name Dialogue
 'What a talented girl !']
 
 @export var transition = [['Hi ! Thank you so much for helping me with my show, you are so nice =) ',
-'Haha, no problem ! (I will ruin you)'], 
+'Haha, no problem ! (I will replace you soon)'], 
 ['Sorry I missed, i will try harder next time','...'],
 ["I wasn't quick enough this time, but please bear with me ","..."],
 ['','']]
