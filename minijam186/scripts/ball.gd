7ab8277822870394f4883df9a64b0f55c0697cb2
charl-baum <charlottebaumgart61@gmail.com>
2025-06-07 14:35:31 +0200

    update trajectoire

--- minijam186/scripts/ball.gd
+++ minijam186/scripts/ball.gd
@@ -3,7 +3,6 @@ extends RigidBody2D
 
 var force = 500.0
 
-@onready var impact_point: Sprite2D = $Impact_point
 
 
 func _ready() -> void:
@@ -12,6 +11,6 @@ func _ready() -> void:
 func throw(angle: float):
 	gravity_scale = 1
 	
-	var coeff_speed_ver = -tan(-angle/180*3.14)
+	var coeff_speed_ver = tan(angle/180*3.14)
 	var velocity = Vector2(1, coeff_speed_ver).normalized()*force
 	apply_impulse(velocity)
