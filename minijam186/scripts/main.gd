c310b37437ca7da23e6eff1741252091b4f65ac3
SolennEstier <solenn.estier@gmail.com>
2025-06-07 18:30:38 +0200

    added timer to speech bubbles

--- minijam186/scripts/main.gd
+++ minijam186/scripts/main.gd
@@ -2,6 +2,7 @@ extends Node2D
 @onready var player_body: CharacterBody2D = $player_body
 @onready var ball: RigidBody2D = $Ball
 @onready var juggler_body: CharacterBody2D = $juggler_body
+@onready var public_bubble_1_timer: Timer = $public_bubble_1_timer
 
 var ball_scene = preload("res://scenes/ball.tscn")
 var start_ball_position: Vector2
@@ -23,7 +24,7 @@ func _on_juggler_body_ball_caught(body) -> void:
 	add_child(new_ball)
 	new_ball.position = start_ball_position
 	active_ball = new_ball
-	public_bubble_1.visible = true
+	public_congratulations()
 	pass # Replace with function body.
 
 func ball_missed(body):
@@ -44,3 +45,10 @@ func _on_player_body_throw_ball(angle, impact_point) -> void:
 func _on_down_boundary_body_entered(body: Node2D) -> void:
 	ball_missed(body)
 	
+func public_congratulations():
+	# TODO : add randomization amongst bubbles and text
+	public_bubble_1.visible = true
+	public_bubble_1_timer.start()
+	
+func _on_public_bubble_1_timer_timeout() -> void:
+	public_bubble_1.visible = false
