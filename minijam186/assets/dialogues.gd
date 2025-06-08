9175aa5206626e540443c7186d18f8be595c0553
SolennEstier <solenn.estier@gmail.com>
2025-06-08 18:34:52 +0200

    added dialogues

--- minijam186/assets/dialogues.gd
+++ minijam186/assets/dialogues.gd
@@ -7,8 +7,8 @@ class_name Dialogue
 
 @export var transition = [['Hi ! Thank you so much for helping me with my show, you are so nice =) ',
 'Haha, no problem ! (Soon, I will replace you)'], 
-["Sorry I didn't catch it, I will try harder next time",'... ()'],
-["I wasn't quick enough this time, but I'm getting better !","..."],
-['',''],
+["Sorry I didn't catch it, I will try harder next time","Ah... it's okay"],
+["I wasn't quick enough this time, but I'm getting better !","You're doing great..."],
+["Sorry, I need to improve my skating. Just wait and see !","(Does she never give up ?)"],
 ['',''],
 ['','']]
